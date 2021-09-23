52b9d2521e51f49aefe6774a0d0cda9b66771193
Mica <mica676@Micas-MacBook-Air.local>
2021-09-22 20:50:02 -0500

    third push

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,17 @@
-var hamburger = document.querySelector(".hamburger");
-	hamburger.addEventListener("click", function(){
-		document.querySelector("body").classList.toggle("active");
-	})
+const menuDisplayButton = document.getElementById ('menu-trigger');
+
+menuDisplayButton.addEventListener("click", displayHamMenu);
+
+const menuCloseButton = document.getElementById ("menu-close");
+console.log(menuCloseButton);
+menuCloseButton.addEventListener("click", hideHamMenu);
+
+
+const hamMenu = document.getElementById ('side-menu');
+function displayHamMenu() {
+  hamMenu.classList.add("show-menu");
+}
+function hideHamMenu() {
+  hamMenu.classList.remove("show-menu");
+}
 
